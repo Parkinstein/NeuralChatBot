639612196e8324a3ffcbfcaa0abe6ca23a2cd2cc
Roman <lntyrd@gmail.com>
2015-11-04 22:54:01 +0300

    Some changes

--- NeuralChatBot/VkDialogConverter/DialogRecord.cs
+++ NeuralChatBot/VkDialogConverter/DialogRecord.cs
@@ -9,6 +9,7 @@ namespace VkDialogConverter
 {
     class DialogRecord
     {
+        public DialogRecord RelatedRecord { get; set; }
         public string Sender { get; set; }
         public string Message { get; set; }
         public List<string> Words
@@ -21,14 +22,14 @@ namespace VkDialogConverter
 
         public const string IgnoreRegularTemplate = @"\d\d\.\d\d\.\d\d";
 
-        public DialogRecord(string sender, string[] message)
+        public DialogRecord(string sender, List<string> message)
         {
             Sender = sender;
 
             StringBuilder sb = new StringBuilder();
             foreach(var s in message)
             {
-                var s1 = s.Replace("\n", "").Replace(",","");
+                var s1 = s.Replace("\n", "").Replace(",","").Replace("\t", "");
                 if (!Regex.IsMatch(s1.Trim(), IgnoreRegularTemplate) &&!string.IsNullOrWhiteSpace(s1))
                 {
                     sb.Append(s1);
@@ -37,7 +38,7 @@ namespace VkDialogConverter
             Message = sb.ToString();
         }
         
-        public void Append(string[] message)
+        public void Append(List<string> message)
         {
             StringBuilder sb = new StringBuilder();
             foreach (var s in message)
--- NeuralChatBot/VkDialogConverter/Program.cs
+++ NeuralChatBot/VkDialogConverter/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Word2Vec.Net;
+using System.IO;
 
 namespace VkDialogConverter
 {
@@ -11,6 +12,17 @@ namespace VkDialogConverter
     {
         static void Main(string[] args)
         {
+            string speaker1 = "Roman";
+            string speaker2 = "Vano";
+            var buffer = new List<string>();
+            using(StreamReader streamReader = new StreamReader(@"C:\TrainingSet\Вано.txt"))
+            {           
+                while (!streamReader.EndOfStream)
+                {
+                    buffer.Add(streamReader.ReadLine()); 
+                }
+            }
+            Console.ReadKey();
         }
     }
 }
