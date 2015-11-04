c6761f70b19dc74aa290f553eafc42aa87f55998
Roman <lntyrd@gmail.com>
2015-11-04 21:52:20 +0300

    DialogRecord Added

---
+++ NeuralChatBot/VkDialogConverter/DialogRecord.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace VkDialogConverter
+{
+    class DialogRecord
+    {
+        public string Sender { get; set; }
+        public string Message { get; set; }
+        public List<string> Words
+        {
+            get
+            {
+                return Message.Split(' ').Where(x=>!string.IsNullOrEmpty(x)&&!string.IsNullOrWhiteSpace(x)).ToList();
+            }
+        }
+
+        public const string IgnoreRegularTemplate = @"\d\d\.\d\d\.\d\d";
+
+        public DialogRecord(string sender, string[] message)
+        {
+            Sender = sender;
+
+            StringBuilder sb = new StringBuilder();
+            foreach(var s in message)
+            {
+                var s1 = s.Replace("\n", "").Replace(",","");
+                if (!Regex.IsMatch(s1.Trim(), IgnoreRegularTemplate) &&!string.IsNullOrWhiteSpace(s1))
+                {
+                    sb.Append(s1);
+                }               
+            }
+            Message = sb.ToString();
+        }
+        
+        public void Append(string[] message)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (var s in message)
+            {
+                var s1 = s.Replace("\n", "");
+                if (!Regex.IsMatch(s1.Trim(), IgnoreRegularTemplate) && !string.IsNullOrWhiteSpace(s1))
+                {
+                    sb.Append(s1);
+                }
+            }
+            Message += sb.ToString();
+        }
+    }
+}
--- NeuralChatBot/VkDialogConverter/Program.cs
+++ NeuralChatBot/VkDialogConverter/Program.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Word2Vec.Net;
 
-namespace GrammarEngine
+namespace VkDialogConverter
 {
     class Program
     {
