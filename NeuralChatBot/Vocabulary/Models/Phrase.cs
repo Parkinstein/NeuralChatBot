1b01ce39369ec817b16f26ec9552546e3bf14f1a
Роман Ульянов <lntyrd@gmail.com>
2015-11-08 18:39:59 +0300

    Added Vocabulary project

---
+++ NeuralChatBot/Vocabulary/Models/Phrase.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vocabulary.Models
+{
+    class Phrase
+    {
+        public string Author { get; set; }
+        public List<Word> Words { get; set; }
+        public Phrase RelatedPhrase { get; set; }
+    }
+}
---
+++ NeuralChatBot/Vocabulary/Models/Word.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vocabulary
+{
+    public class Word
+    {
+        public string Value { get; set; }
+        public double[] Coords { get; set; }
+    }
+}
